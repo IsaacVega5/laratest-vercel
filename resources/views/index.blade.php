18405eab4751c787e43ff446241dbf14e6dec621
IsaacVega <isaacvega361@gmail.com>
2023-07-10 19:14:05 -0400

    Se crea un index de prueba para ser mostrado en la página home

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Emprende utal</title>
+</head>
+<body>
+    <style>
+        body{
+            display: flex;
+            height: 100vh;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        .titulo{
+            display: flex;
+            gap: 10px
+        }
+    </style>
+    <div class="titulo">
+        <h4 style="margin:auto;">Emprende</h4>
+        <hr>
+        <h1>Utal</h1>
+    </div>
+    <p>Página en construcción</p>
+</body>
+</html>
